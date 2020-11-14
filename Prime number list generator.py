9786cf316c2d8f67734afe11f90912d07af6b8ec
Stanley Shikwambi <55202628+shikwambipolly@users.noreply.github.com>
2020-11-14 12:46:19 +0200

    Update Prime number list generator.py

--- Prime number list generator.py
+++ Prime number list generator.py
@@ -1,9 +1,10 @@
 import time
 import sys
 
+print("This program will output all prime numbers from 2 to upper bound.")
 
 try:
-    x = int(input("Enter your number: "))
+    x = int(input("Enter your upper bound: "))
 except ValueError:
     print("You have not entered a number.")
     time.sleep(3)
@@ -24,7 +25,7 @@ def pmchecker(x):
         return False
 
 def pmlister(x):
-    for i in range(4, x):
+    for i in range(2, x):
         if pmchecker(i) is True:
             pm.append(i)
     print(pm)
